4e3a8416b86c97cb022a3a3d3d18d3965f7b6863
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-02-26 19:23:18 +0100

    Update calendar.py

--- calendar.py
+++ calendar.py
@@ -1,45 +1,44 @@
+# -*- coding: utf-8 -*-
+
 from VALID import ns, OKI
 import calendar
-import subprocess
 
-def AB(n):
+def AB(n): #VALIDAR DATO DE ENTRADA OPCIÓN (A/B)
     while n!=("A") and n!=("B"):
         n=input("Escriba solo \'A\' o \'B\' segun su opción: ")
     return n
 
-def año_valido(n):
+def anno_valido(n): #VALIDAR DATO DE ENTRADA (AÑO)
     while n<1 or n>9999:
         n=OKI(input("Introduzca un año valido: "))
     return n
 
-def mes_valido(n):
+def mes_valido(n): #VALIDAR DATO DE ENTRADA (MES)
      while n<1 or n>12:
          n=OKI(input("Introduzca un mes valido: "))
      return n
 
-
 while True:
-    print("CALENDARIOS")
+    print("\n------------------------------CALENDARIOS------------------------------\n")
     print("""Escoja una opción:
-A)Ver los calendarios correspondientes a un determinado año
-B)Ver el calendario correspondiente a un año y mes determinados
+A)Ver los calendarios correspondientes a un determinado año.
+B)Ver el calendario correspondiente a un año y mes determinados.
 """)
-    opcion=AB(input("Introduzca su opción: "))
+    opcion=AB(input("Introduzca su opción(A/B): "))
     if opcion==("A"):
-        año=año_valido(OKI(input("Introduce el año cuyos calendarios desea ver: ")))
+        anno=anno_valido(OKI(input("Introduce el año cuyos calendarios desea ver: ")))
         cl=calendar.TextCalendar()
-        calendario=cl.formatyear(año)
+        calendario=cl.formatyear(anno)
         print("")
         print(calendario)
     else:
-        año=año_valido(OKI(input("Introduzca el año: ")))
+        anno=anno_valido(OKI(input("Introduzca el año: ")))
         mes=mes_valido(OKI(input("Introduzca mes: ")))
         cl=calendar.TextCalendar()
-        calendario=cl.formatmonth(año,mes)
+        calendario=cl.formatmonth(anno,mes)
         print("")
         print(calendario)
-    conti=ns(input("¿Desea continuar?: "))
+        
+    conti=ns(input("¿Desea continuar?(n/s): "))
     if conti==("n"):
         break
-    else:
-        subprocess.call(["cmd.exe","/C","cls"])
